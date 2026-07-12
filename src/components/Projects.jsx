d0b473bc6772a0d2d6fc545097d62215acd200c3
Gowthami <gowthamiputta2008.gmail.com>
2026-07-12 21:24:50 +0530

    Updated resume

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,13 +2,13 @@ export default function Projects() {
   return (
     <section className="projects" id="projects">
       <h2>Projects</h2>
-      
+
       <div className="project">
         <h3>Hostel management system wedsite</h3>
         <p>To my college for Hostel management</p>
       </div>
       <div className="project">
-        <h3>VLSI Portfolio Website</h3>
+        <h3>Portfolio Website</h3>
         <p>HTML and CSS and React based responsive personal portfolio.</p>
       </div>
     </section>
